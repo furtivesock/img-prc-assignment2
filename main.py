720761e9f011ea8afec145ffd35fa629382c9cf1
tom.mansion <tomansion@yahoo.fr>
2021-10-13 22:48:33 +0200

    applied vscode auto indent

--- main.py
+++ main.py
@@ -23,14 +23,17 @@ OUTPUT_FOLDER = "outputs"
 
 MAX_KERNEL_LENGTH = 9
 
+# Row
 R_MIN = 1
 R_MAX = 100
 
+# Column
 C_MIN = 1
 C_MAX = 100
 
 RAD_MIN = 3
 
+# Circle selection
 N_CIRCLES = 4
 DELTA_R = 3
 DELTA_C = 3
@@ -38,6 +41,7 @@ DELTA_RAD = 3
 
 CIRCLE_THICKNESS = 1
 MARKER_SIZE = 5
+
 
 def remove_noise(img) -> np.array:
     """Remove noise (if it exists) with median blur to make circle detection easier
@@ -48,7 +52,8 @@ def remove_noise(img) -> np.array:
     Returns:
         np.array            : Cleaned image
     """
-    return cv.medianBlur(src=img, ksize=5)   
+    return cv.medianBlur(src=img, ksize=5)
+
 
 def sobelize(img) -> np.array:
     """Apply Sobel filter on an image for edge detection
@@ -59,7 +64,7 @@ def sobelize(img) -> np.array:
     Returns:
         np.array            : Output image
     """
-    # Compute horizontal (grad_x) and vertical (grad_y) derivatives 
+    # Compute horizontal (grad_x) and vertical (grad_y) derivatives
     grad_x = cv.Sobel(src=img, ddepth=cv.CV_64F, dx=1, dy=0)
     grad_y = cv.Sobel(src=img, ddepth=cv.CV_64F, dx=0, dy=1)
 
@@ -67,6 +72,7 @@ def sobelize(img) -> np.array:
     abs_grad_x = cv.convertScaleAbs(grad_x)
     abs_grad_y = cv.convertScaleAbs(grad_y)
     return cv.addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0)
+
 
 def is_local_maximum(acc, i, j, k, shape) -> bool:
     """Define if current element in 3D-accumulator is local maximum
@@ -87,16 +93,20 @@ def is_local_maximum(acc, i, j, k, shape) -> bool:
     if ref == 0:
         return False
 
-    i_slice = np.s_[i-1:i+2] if i > 0 and i < shape[0] - 1 else (np.s_[i:i+2] if i == 0 else np.s_[i-1:])
-    j_slice = np.s_[j-1:j+2] if j > 0 and j < shape[1] - 1 else (np.s_[j:j+2] if j == 0 else np.s_[j-1:])
-    k_slice = np.s_[k-1:k+2] if k > 0 and k < shape[2] - 1 else (np.s_[k:k+2] if k == 0 else np.s_[k-1:])
+    i_slice = np.s_[i - 1:i + 2] if i > 0 and i < shape[0] - \
+        1 else (np.s_[i:i + 2] if i == 0 else np.s_[i - 1:])
+    j_slice = np.s_[j - 1:j + 2] if j > 0 and j < shape[1] - \
+        1 else (np.s_[j:j + 2] if j == 0 else np.s_[j - 1:])
+    k_slice = np.s_[k - 1:k + 2] if k > 0 and k < shape[2] - \
+        1 else (np.s_[k:k + 2] if k == 0 else np.s_[k - 1:])
 
     indices = (i_slice, j_slice, k_slice)
 
     local_area = acc[indices]
-       
+
     maximum = np.amax(local_area)
     return maximum == ref
+
 
 def get_detected_circles(acc, N_circles=N_CIRCLES) -> list:
     """Get N_circles detected circles from computed accumulator
@@ -116,9 +126,11 @@ def get_detected_circles(acc, N_circles=N_CIRCLES) -> list:
         for j in range(cols):
             for k in range(depth):
                 if is_local_maximum(acc, i, j, k, (rows, cols, depth)):
-                    local_maxima.append({"value": acc[i, j, k], "r": j, "c": i, "rad": k})
+                    local_maxima.append(
+                        {"value": acc[i, j, k], "r": j, "c": i, "rad": k})
 
-    sorted_maxima = sorted(local_maxima, key=lambda maximum: maximum["value"])[::-1]
+    sorted_maxima = sorted(
+        local_maxima, key=lambda maximum: maximum["value"])[::-1]
 
     detected_circles = []
 
@@ -128,15 +140,16 @@ def get_detected_circles(acc, N_circles=N_CIRCLES) -> list:
         if len(detected_circles) == N_circles:
             break
         for circle in detected_circles:
-            if (abs(circle['r'] - sorted_maxima[i]['r']) < DELTA_R 
-                and abs(circle['c'] - sorted_maxima[i]['c']) < DELTA_C 
-                and abs(circle['rad'] - sorted_maxima[i]['rad']) < DELTA_RAD):
+            if (abs(circle['r'] - sorted_maxima[i]['r']) < DELTA_R
+                and abs(circle['c'] - sorted_maxima[i]['c']) < DELTA_C
+                    and abs(circle['rad'] - sorted_maxima[i]['rad']) < DELTA_RAD):
                 ignore = True
                 break
         if not ignore:
             detected_circles.append(sorted_maxima[i])
 
     return detected_circles
+
 
 def hough_circles(img, rows, cols, r_min, r_max, c_min, c_max, rad_min, rad_max) -> list:
     """Fill the accumulator for each possible circle that passes through an edge pixel
@@ -153,9 +166,10 @@ def hough_circles(img, rows, cols, r_min, r_max, c_min, c_max, rad_min, rad_max)
         rad_max (int)   : Maximum circle radius
 
     Returns:
-        list            : List of N_circles detected circles 
+        list            : List of N_circles detected circles
     """
-    acc = np.zeros((r_max - r_min + 1, c_max - c_min + 1, int(rad_max - rad_min) + 1), dtype=float)
+    acc = np.zeros((r_max - r_min + 1, c_max - c_min + 1,
+                    int(rad_max - rad_min) + 1), dtype=float)
 
     print("Filling the accumulator...")
     for y in range(0, rows):
@@ -163,15 +177,17 @@ def hough_circles(img, rows, cols, r_min, r_max, c_min, c_max, rad_min, rad_max)
             print(f"Current pixel : {y}, {x}", end="\r")
             if img[y, x] > 0:
                 for r in range(r_min - 1, r_max):
-                    for c in range (c_min - 1, c_max):
+                    for c in range(c_min - 1, c_max):
                         rad = int(math.sqrt(((y - r) ** 2) + ((x - c) ** 2)))
                         if rad >= rad_min and rad < rad_max:
-                            acc[r - r_min, c - c_max, rad - rad_min] += img[y, x]/rad
+                            acc[r - r_min, c - c_max, rad -
+                                rad_min] += img[y, x] / rad
 
     detected_circles = get_detected_circles(acc)
 
     return detected_circles
-    
+
+
 def draw_circles(img, circles, r_min, c_min, rad_min, thickness=CIRCLE_THICKNESS, marker_size=MARKER_SIZE) -> np.array:
     """Draw circles (with markers on the center) to an image
 
@@ -180,24 +196,36 @@ def draw_circles(img, circles, r_min, c_min, rad_min, thickness=CIRCLE_THICKNESS
         circles (list)      : List of circles parameters (format: {"r": int, "c": int, "rad": int})
         r_min (int)         : Minimum r axis value (used for gap as accumulator starts by 0 on each axis)
         c_min (int)         : Minimum c axis value
-        rad_min (int)       : Minimum circle radius 
+        rad_min (int)       : Minimum circle radius
         thickness (int)     : Circle line thickness
         marker_size (int)   : Centred marker size
 
     Returns:
-        np.array            : Output image with drawn circles 
+        np.array            : Output image with drawn circles
     """
     modified_img = img.copy()
 
     for circle in circles:
-        modified_img = cv.circle(img=modified_img, center=(circle['r'] + r_min - 1, circle['c'] + c_min - 1), radius=circle['rad'] + rad_min + 1, color=(0, 0, 255), thickness=thickness)
-        modified_img = cv.drawMarker(position=(circle['r'], circle['c']), img=modified_img, color=(0, 0, 255), markerType=cv.MARKER_CROSS, markerSize=marker_size)
-    
+        modified_img = cv.circle(img=modified_img, center=(
+            circle['r'] + r_min - 1, circle['c'] + c_min - 1), radius=circle['rad'] + rad_min + 1, color=(0, 0, 255), thickness=thickness)
+        modified_img = cv.drawMarker(position=(circle['r'], circle['c']), img=modified_img, color=(
+            0, 0, 255), markerType=cv.MARKER_CROSS, markerSize=marker_size)
+
     return modified_img
 
-parser = argparse.ArgumentParser(description="Detect circles on images in /images folder")
-parser.add_argument("--images", type=str, nargs='+', default=IMAGES, help="an image to detect circles")
-parser.add_argument("-s", "--save", dest="save", action="store_true", default=False, help="save output images into /output folder (default: disabled)")
+
+parser = argparse.ArgumentParser(
+    description="Detect circles on images in /images folder")
+parser.add_argument("--images",
+                    type=str,
+                    nargs='+',
+                    default=IMAGES,
+                    help="an image to detect circles")
+parser.add_argument("-s", "--save",
+                    dest="save",
+                    action="store_true",
+                    default=False,
+                    help="save output images into /output folder (default: disabled)")
 
 if __name__ == "__main__":
     args = parser.parse_args()
@@ -207,7 +235,7 @@ if __name__ == "__main__":
     for image_name in images:
         img = cv.imread(f"{IMAGES_FOLDER}/{image_name}")
         print(f"CURRENT IMAGE : {image_name}")
-        
+
         # Apply median blur to remove noise
         cleaned_img = remove_noise(cv.cvtColor(img, cv.COLOR_BGR2GRAY))
 
@@ -223,9 +251,11 @@ if __name__ == "__main__":
         # Compute maximum radius using Pythagorean theorem
         rad_max = math.sqrt(r_max ** 2 + c_max ** 2)
         rad_min = RAD_MIN
-        detected_circles = hough_circles(filtered_img, rows, cols, r_min, r_max, c_min, c_max, rad_min, rad_max)
+        detected_circles = hough_circles(
+            filtered_img, rows, cols, r_min, r_max, c_min, c_max, rad_min, rad_max)
 
-        drawn_image = draw_circles(img, detected_circles, r_min, c_min, rad_min)
+        drawn_image = draw_circles(
+            img, detected_circles, r_min, c_min, rad_min)
 
         plt.figure(num=image_name)
         plt.subplot(2, 2, 1)
@@ -246,7 +276,7 @@ if __name__ == "__main__":
             if not os.path.isdir(OUTPUT_FOLDER):
                 os.mkdir(OUTPUT_FOLDER)
             status = cv.imwrite(f"{OUTPUT_FOLDER}/{image_name}", drawn_image)
-        
+
         plt.tight_layout()
         print("Showing outputs")
         plt.show()
